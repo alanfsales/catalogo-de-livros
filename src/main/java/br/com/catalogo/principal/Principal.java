0a88a3d7dc7c74d1f6c5222368f40a464a099170
AlanFSales <alanfsales27@gmail.com>
2024-05-17 23:47:25 -0300

    Adiciona a classe ConsultaAPI

--- src/main/java/br/com/catalogo/principal/Principal.java
+++ src/main/java/br/com/catalogo/principal/Principal.java
@@ -1,5 +1,8 @@
 package br.com.catalogo.principal;
 
+import br.com.catalogo.service.ConsultaAPI;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Principal {
 
     public void exibeMenu(){
@@ -8,6 +11,12 @@ public class Principal {
                 2 - Listar livros registrados
                 
                 """;
-        System.out.println(menu);
+        ConsultaAPI consultaAPI = new ConsultaAPI();
+        String json = consultaAPI.buscar("https://gutendex.com/books/?search=dom+casmurro");
+
+        System.out.println(json);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.
     }
 }
